177077a19dfca20bf1597d7891bda99a3bf10677
itsyous <youssef.rabih1995@gmail.com>
2023-12-11 05:59:19 -0800

    loop

---
+++ shell_loop.c
@@ -0,0 +1,40 @@
+#include "shell.h"
+/**
+ * The shell_loop function is responsible for the main loop of the shell,
+ * where it repeatedly prompts for user input, reads the command, and executes it.
+ *
+ * Return: void.
+ */
+void shell_loop()
+{
+	char *command = NULL;
+	size_t command_size = 0;
+	ssize_t input_length;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))  /* Check if input is from a terminal*/
+        {
+            display_prompt();
+        }
+        input_length = getline(&command, &command_size, stdin);
+        if (input_length == -1)
+        {
+            if (isatty(STDIN_FILENO)) /* Handle end of file (Ctrl+D) or error */
+            {
+                myprint("\n");
+            }
+            break;
+        }
+		/* Remove trailing newline character */
+		if (command[input_length - 1] == '\n')
+		{
+			command[input_length - 1] = '\0';
+		}
+		if (_strspn(command, " \t\n\r\a") < _strlen(command))  
+        {
+            execute_command(command);
+        }
+	}
+	free(command);
+}
