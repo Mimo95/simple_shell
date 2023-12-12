7ff4ce914edcb4bb1fc654566978a5786b9c980e
itsyous <youssef.rabih1995@gmail.com>
2023-12-12 15:47:20 -0800

    execute

--- ex_command.c
+++ ex_command.c
@@ -8,42 +8,44 @@
 void execute_command(char *command)
 {
 	if (_strcmp(command, "exit") == 0)
-    {
-		free(command);
-        exit(EXIT_SUCCESS);
-    }
-	else if (_strcmp(command, "env") == 0) 
 	{
-        char **env_var = environ;
-        while (*env_var != NULL) 
+		free(command);
+		exit(EXIT_SUCCESS);
+	}
+	else if (_strcmp(command, "env") == 0)
+	{
+		char **env_var = environ;
+
+		while (*env_var != NULL)
 		{
-            myprint(*env_var);
-            myprint("\n");
-            env_var++;
-        }
-        return;
-    }
+			myprint(*env_var);
+			myprint("\n");
+			env_var++;
+		}
+		return;
+	}
 	else
 	{
-	    pid_t pid = fork(); /* Create a new process by forking */
-        if (pid < 0)
-        {
-            perror("Fork failed");
-            exit(EXIT_FAILURE);
-        }
-        if (pid == 0)
-        {
-            char **args = tokenize(command);
-		    if (args[0] == NULL)
-                free_tokens(args);
-                exit(EXIT_FAILURE);
+		pid_t pid = fork(); /* Create a new process by forking */
 
-            execve(args[0], args, environ);
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-        else
-            wait(NULL);
-    }
+		if (pid < 0)
+		{
+			perror("Fork failed");
+			exit(EXIT_FAILURE);
+		}
+		if (pid == 0)
+		{
+			char **args = tokenize(command);
+
+				if (args[0] == NULL)
+					free_tokens(args);
+				exit(EXIT_FAILURE);
+				execve(args[0], args, environ);
+				perror("execve");
+				exit(EXIT_FAILURE);
+		}
+		else
+			wait(NULL);
+	}
 }
 
