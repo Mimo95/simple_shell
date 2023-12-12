7f6f2f9dbb27b9ed62b06f173779b787efdc35eb
itsyous <youssef.rabih1995@gmail.com>
2023-12-12 14:33:58 -0800

    tokenizing

--- tokenize.c
+++ tokenize.c
@@ -1,40 +1,41 @@
 #include "shell.h"
 /**
- * tokenize - Tokenize a command string into an array of tokens.
+ * tokenize - Tokenize a command string into
+ * an array of tokens
  * @command: The command string to tokenize.
  *
  * Return: An array of strings (tokens).
  */
 char **tokenize(char *command)
 {
-    char **tokens = malloc(sizeof(char *) * TOKEN_BUFFER_SIZE);
-    char *token;
-    int index = 0;
+	char **tokens = malloc(sizeof(char *) * TOKEN_BUFFER_SIZE);
+	char *token;
+	int index = 0;
 
-    if (!tokens)
-    {
-        perror("malloc failed");
-        exit(EXIT_FAILURE);
-    }
-    token = strtok(command, " \t\n"); /* Tokenize based on space and tabs */
-    while (token != NULL)
-    {
-        tokens[index] = _strdup(token);
-        if (!tokens[index])
-        {
-            perror("strdup failed");
-            exit(EXIT_FAILURE);
-        }
-        index++;
+	if (!tokens)
+	{
+		perror("malloc failed");
+		exit(EXIT_FAILURE);
+	}
+	token = strtok(command, " \t\n");
+	while (token != NULL)
+	{
+		tokens[index] = _strdup(token);
+		if (!tokens[index])
+		{
+			perror("strdup failed");
+			exit(EXIT_FAILURE);
+		}
+		index++;
 
-        if (index >= TOKEN_BUFFER_SIZE)
-        {
-            perror("Too many tokens");
-            exit(EXIT_FAILURE);
-        }
-        token = strtok(NULL, " \t\n");
-    }
-    tokens[index] = NULL; /* Add a NULL terminator at the end of the tokens array */
+		if (index >= TOKEN_BUFFER_SIZE)
+		{
+			perror("Too many tokens");
+			exit(EXIT_FAILURE);
+		}
+		token = strtok(NULL, " \t\n");
+	}
+	tokens[index] = NULL;
 
-    return (tokens);
+	return (tokens);
 }
