438b8faa28177da62118422e332a3d59cf7ea8f6
itsyous <youssef.rabih1995@gmail.com>
2023-12-12 15:50:41 -0800

    free

--- free_tok.c
+++ free_tok.c
@@ -8,11 +8,12 @@
  */
 void free_tokens(char **tokens)
 {
-    int i = 0;
-    while (tokens[i] != NULL)
-    {
-        free(tokens[i]);
-        i++;
-    }
-    free(tokens);
+	int i = 0;
+
+	while (tokens[i] != NULL)
+	{
+		free(tokens[i]);
+		i++;
+	}
+	free(tokens);
 }
