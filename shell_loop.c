42ce15572ce124a826df66564db7dd804f1f2969
itsyous <youssef.rabih1995@gmail.com>
2023-12-12 15:28:20 -0800

    loop

--- shell_loop.c
+++ shell_loop.c
@@ -1,11 +1,12 @@
 #include "shell.h"
 /**
- * The shell_loop function is responsible for the main loop of the shell,
- * where it repeatedly prompts for user input, reads the command, and executes it.
+ * shell_loop - function is responsible for the main loop of the shell,
+ * where it repeatedly prompts for user input,
+ * reads the command and execute it
  *
  * Return: void.
  */
-void shell_loop()
+void shell_loop(void)
 {
 	char *command = NULL;
 	size_t command_size = 0;
@@ -14,27 +15,27 @@ void shell_loop()
 	while (1)
 	{
 		if (isatty(STDIN_FILENO))  /* Check if input is from a terminal*/
-        {
-            display_prompt();
-        }
-        input_length = getline(&command, &command_size, stdin);
-        if (input_length == -1)
-        {
-            if (isatty(STDIN_FILENO)) /* Handle end of file (Ctrl+D) or error */
-            {
-                myprint("\n");
-            }
-            break;
-        }
+		{
+			display_prompt();
+		}
+		input_length = getline(&command, &command_size, stdin);
+		if (input_length == -1)
+		{
+			if (isatty(STDIN_FILENO)) /* Handle end of file (Ctrl+D) or error */
+			{
+				myprint("\n");
+			}
+			break;
+		}
 		/* Remove trailing newline character */
 		if (command[input_length - 1] == '\n')
 		{
 			command[input_length - 1] = '\0';
 		}
-		if (_strspn(command, " \t\n\r\a") < _strlen(command))  
-        {
-            execute_command(command);
-        }
+		if (_strspn(command, " \t\n\r\a") < _strlen(command))
+		{
+		execute_command(command);
+	}
 	}
 	free(command);
 }
