b4ab991fa050d259a8f9dbd4701807b45444f455
Mohamed HAJJOUBI <63694974+Mimo95@users.noreply.github.com>
2023-12-13 05:18:39 +0100

    Update ex_command.c

--- ex_command.c
+++ ex_command.c
@@ -8,44 +8,51 @@
 void execute_command(char *command)
 {
 	if (_strcmp(command, "exit") == 0)
-	{
+    {
 		free(command);
-		exit(EXIT_SUCCESS);
-	}
-	else if (_strcmp(command, "env") == 0)
+        /* Handle 'exit' command*/
+        exit(EXIT_SUCCESS);
+    }
+	else if (_strcmp(command, "env") == 0) 
 	{
-		char **env_var = environ;
-
-		while (*env_var != NULL)
+        /* If the command is "env", print the current environment variables*/
+        char **env_var = environ;
+        while (*env_var != NULL) 
 		{
-			myprint(*env_var);
-			myprint("\n");
-			env_var++;
-		}
-		return;
-	}
+            myprint(*env_var);
+            myprint("\n");
+            env_var++;
+        }
+        return;
+    }
 	else
 	{
-		pid_t pid = fork(); /* Create a new process by forking */
+	pid_t pid = fork(); /* Create a new process by forking */
 
-		if (pid < 0)
-		{
-			perror("Fork failed");
-			exit(EXIT_FAILURE);
-		}
-		if (pid == 0)
-		{
-			char **args = tokenize(command);
-
-				if (args[0] == NULL)
-					free_tokens(args);
-				exit(EXIT_FAILURE);
-				execve(args[0], args, environ);
-				perror("execve");
-				exit(EXIT_FAILURE);
-		}
-		else
-			wait(NULL);
-	}
+    if (pid < 0)
+    {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    }
+    if (pid == 0)
+    {
+        /* Child process */
+        char **args = tokenize(command);
+		if (args[0] == NULL)
+        {
+            free_tokens(args);
+            exit(EXIT_FAILURE);
+        }
+		/* Execute the command */
+        execve(args[0], args, environ);
+        /* If execve returns, an error occurred */
+        perror("execve");
+        exit(EXIT_FAILURE);
+    }
+    else
+    {
+        /* Parent process */
+        wait(NULL);
+    }
+    }
 }
-
